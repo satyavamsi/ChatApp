f06897f22e495823d743a3fafd9dae21077e919f
Satya Vamsi <satya.vamsi@alphanodus.com>
2020-08-11 03:19:28 +0530

    finished message display from db

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/channel/:channelId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>WELCOME</h1>
+            </Route>
+          </Switch>
+          {/* React-Router -> Chat Screen */}
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/Chat.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+
+import db from './firebase';
+
+import './Chat.css'
+
+import Message from './Message'
+
+function Chat() {
+
+    const { channelId } = useParams();
+
+    const [channelDetails, setChannelDetails] = useState(null);
+    const [channelMessages, setchannelMessages] = useState([]);
+
+    useEffect(() => {
+        db.collection('channels').doc(channelId)
+            .onSnapshot(snapshot => {
+                setChannelDetails(snapshot.data());
+            });
+        db.collection('channels').doc(channelId)
+            .collection('messages')
+            .orderBy('timestamp', 'asc')
+            .onSnapshot((snapshot) =>
+                setchannelMessages(
+                    snapshot.docs.map(doc => doc.data())
+                )
+            );
+    }, [channelId])
+
+
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <div className="chat__headerLeft">
+                    <h4 className="chat__channelName">
+                        <strong># {channelDetails?.name}</strong>
+                        <StarBorderOutlinedIcon />
+                    </h4>
+                </div>
+                <div className="chat__headerRight">
+                    <p><InfoOutlinedIcon /> Details </p>
+                </div>
+            </div>
+            <div className="chat__messages">
+                {channelMessages.map(message => (
+                    <Message
+                        message={message.message}
+                        timestamp={message.timestamp}
+                        user={message.user}
+                        userImage={message.userImage} />
+                ))}
+
+            </div>
+        </div>
+    )
+}
+
+export default Chat
