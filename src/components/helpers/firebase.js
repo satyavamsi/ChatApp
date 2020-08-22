c3dd81c6442a61edb2ef2d76e29703fcbd6ee499
Satya Vamsi <satya.vamsi@alphanodus.com>
2020-08-22 15:47:23 +0530

    added og tags and sample site image

--- src/components/helpers/firebase.js
+++ src/components/helpers/firebase.js
@@ -2,6 +2,14 @@ import firebase from 'firebase';
 
 //firebase config
 const firebaseConfig = {
+    apiKey: "AIzaSyBleefU7X6aYDvczHFLVkttSEHz6snxOi4",
+    authDomain: "chat-app-17bf8.firebaseapp.com",
+    databaseURL: "https://chat-app-17bf8.firebaseio.com",
+    projectId: "chat-app-17bf8",
+    storageBucket: "chat-app-17bf8.appspot.com",
+    messagingSenderId: "533056619648",
+    appId: "1:533056619648:web:41878c948f51cb2f22d265",
+    measurementId: "G-9R7KXP3YBF"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
